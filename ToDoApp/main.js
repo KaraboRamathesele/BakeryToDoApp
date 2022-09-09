a97aa073d11e364036ccda2b7074b2ff72476310
KaraboRamathesele <karaboramathesele8115@gmail.com>
2022-09-09 09:19:58 +0200

    Temporary local storae

---
+++ ToDoApp/main.js
@@ -0,0 +1,53 @@
+
+// Order Class: Represents an order to be made
+class Order {
+    constructor(order, customer, date) {
+      this.order = order;
+      this.customer = customer;
+      this.date = date;
+    }
+  }
+  
+  // Table interface class: Handle table tasks
+  class Table {
+    static displayOrders() {
+        const StoredOrders = [
+            {
+                order: 'Vanilla Cake',
+                customer: 'Maria',
+                date: '2022-08-08'
+            },
+            {
+                order: 'Red Velvet Cake',
+                customer: 'Arthur',
+                date: '2022-08-09'
+            }
+        ];
+
+        const bakeryOrders = StoredOrders;
+    }
+  }
+ 
+  
+  // Store Class: Handle local Storage
+  
+  // Event: Display Order
+  
+  // Event: Add a order
+  
+    // Prevent actual submit
+    
+  
+    // Get form values
+    
+    // Validate
+    
+  // Event: Remove a Order
+  
+    // Remove book from Table
+    
+  
+    // Remove book from store
+   
+  
+    // Show success message
